9f76fedcbf27e98a3181dea1215ed9ddb7e56772
kaitomc276 <tandungvuong2012@gmail.com>
2025-04-05 20:53:32 +0700

    Update kaitoMC (1).lua

--- kaitoMC (1).lua
+++ kaitoMC (1).lua
@@ -1,53 +1,65 @@
-local Rayfield = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
+local ScreenGui = Instance.new("ScreenGui")
+local ImageButton = Instance.new("ImageButton")
+local UICorner = Instance.new("UICorner")
 
-local Window = Rayfield:CreateWindow({
-   Name = "ngu",
-   LoadingTitle = "Rayfield Interface Suite",
-   LoadingSubtitle = "by name",
-   ConfigurationSaving = {
-      Enabled = true,
-      FolderName = ngu
-      FileName = "RBS"
-   },
-   Discord = {
-      Enabled = false,
-      Invite = "noinvitelink", 
-      RememberJoins = false  
-   },
-   KeySystem = false, 
-   KeySettings = {
-      Title = "ngu",
-      Subtitle = "Key System",
-      Note = "",
-      FileName = "Key",
-      SaveKey = true, 
-      GrabKeyFromSite = false, 
-      Key = {"Hello"} 
-   }
+ScreenGui.Parent = game.CoreGui
+ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+
+ImageButton.Parent = ScreenGui
+ImageButton.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+ImageButton.BorderSizePixel = 0
+ImageButton.Position = UDim2.new(0.10615778, 0, 0.16217947, 0)
+ImageButton.Size = UDim2.new(0, 40, 0, 40)
+ImageButton.Draggable = true
+ImageButton.Image = "http://www.roblox.com/asset/?id=133377336297183"
+
+UICorner.CornerRadius = UDim.new(1, 10)
+UICorner.Parent = ImageButton
+
+ImageButton.MouseButton1Down:Connect(function()
+    game:GetService("VirtualInputManager"):SendKeyEvent(true, Enum.KeyCode.End, false, game)
+end)
+
+local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
+repeat wait() until game:IsLoaded()
+
+local Window = Fluent:CreateWindow({
+    Title = "Kun Roblox Tổng Hợp",
+    SubTitle = "Blox Fruit",
+    TabWidth = 157,
+    Size = UDim2.fromOffset(500, 320),
+    Acrylic = true,
+    Theme = "Amethyst",
+    MinimizeKey = Enum.KeyCode.End
 })
 
-local MainTab = Window:CreateTab("Fruits", nil) -- Title, Image = "rbxassetid://83190276951914",
-local MainSection = MainTab:CreateSection("Blox Fruits")
+local Tabs = {
+    Main = Window:AddTab({ Title = "Information" }),
+    Main1 = Window:AddTab({ Title = "Fram" }),
+}
 
-Rayfield:Notify({
-   Title = "You executed the script!",
-   Content = "Thanks for using RetributionMK8's Blox Fruits Script",
-   Duration = 5,
-   Image = nil,
-   Actions = { -- Notification Buttons
-      Ignore = {
-         Name = "Okay!",
-         Callback = function()
-         print("The user tapped Okay!")
-      end
-   },
-},
+Tabs.Main1:AddButton({
+    Title = "Redz Hub",
+    Description = "",
+    Callback = function()
+        local Settings = {
+            JoinTeam = "Pirates", -- Pirates/Marines
+            Translator = true     -- true/false
+        }
+
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/newredz/BloxFruits/refs/heads/main/Source.luau"))(Settings)
+    end
 })
 
-local Button = MainTab:CreateButton({
-   Name = "Find Fruits",
-   Callback = function()
-loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
-   end,
+Tabs.Main:AddButton({
+    Title = "YouTube KunRoblox",
+    Description = "",
+    Callback = function()
+        setclipboard("https://youtube.com/@kunrobloxreal?si=NC8cDwiuDTL-cpWJ")
+    end
 })
 
+Tabs.Main:AddParagraph({
+    Title = "KunRoblox Mãi Đỉnh",
+    Content = "Không Like Và Không Subscribe = Gay"
+})
